8d894deff33e538144747841f8fb95b108f803fa
kayla nieto <kaylanieto01@gmail.com>
2018-11-24 20:58:18 +0000

    add content to sports html,css,js

--- sports.js
+++ sports.js
@@ -1,17 +1,14 @@
 var slideIndex = 0;
 showSlides();
 function showSlides() {
-    var i;
     var slides = document.getElementsByClassName("myimages");
-    //   var slides= img1,img2,img3
     for (var i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    slideIndex++;
+    slideIndex++
     if (slideIndex >= slides.length) {
         slideIndex = 0;
     }
     slides[slideIndex].style.display = "block";
-    setTimeout(showSlides, 3000);
-
+    setTimeout(showSlides, 4000);
 }
